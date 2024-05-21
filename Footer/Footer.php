7dc64776f633297724835787b6335f29f8adf1d1
Alex_Cil <alejandro.ciljimenez@gmail.com>
2024-05-21 17:23:46 +0200

    creacion index y mejoras a footer y header

---
+++ Footer/Footer.php
@@ -0,0 +1,18 @@
+    <footer class="fixed-bottom">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary navbar-custom">
+            <div class="container-fluid">
+                <div class="row container-fluid m-1">
+                    <div class="col-12 d-flex flex-wrap justify-content-around p-2">
+                    <button type="button" class="btn btn-outline-dark m-2">Home</button>
+                    <button type="button" class="btn btn-outline-dark m-2">Message</button>
+                    <button type="button" class="btn btn-outline-dark m-2">Search</button>
+                    <button type="button" class="btn btn-outline-dark m-2">Profile</button>
+                    <button type="button" class="btn btn-outline-dark m-2">Search Company</button>
+                    <button type="button" class="btn btn-outline-dark m-2">Classification</button>
+                    <button type="button" class="btn btn-outline-dark m-2">Daily challenges</button>
+
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </footer>
--- head/head.php
+++ head/head.php
@@ -1,28 +1,14 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
-
-<body>
-    <header>
+    <header class="fixed-top">
         <nav class="navbar navbar-expand-lg bg-body-tertiary navbar-custom">
-            <div class="container-fluid">
+            <div class="container-fluid d-flex flex-wrap">
                 <div class="row container-fluid m-1">
-                    <div class="col-4">
+                    <div class="col-12 d-flex justify-content-between align-content-center flex-wrap p-5">
+
                         <!-- El metodo onclick fue creado con chatGPT -->
-                        <button class="btn btn-secondary btn-back mt-1" onclick="window.history.back()">
+                        <button class="btn btn-outline-dark mt-1" onclick="window.history.back()">
                         <</button>
-                    </div>
-                    <div class="col-4">
                         <h2 class="text-center">netgram</h2>
-                    </div>
-                    <div class="col-4 d-flex justify-content-end">
-                        <button class="btn btn-secondary btn-back mt-1">
+                        <button class="btn btn-outline-dark mt-1">
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-chat-fill" viewBox="0 0 16 16">
                                 <path d="M8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6-.097 1.016-.417 2.13-.771 2.966-.079.186.074.394.273.362 2.256-.37 3.597-.938 4.18-1.234A9 9 0 0 0 8 15" />
                             </svg></button>
@@ -31,7 +17,3 @@
             </div>
         </nav>
     </header>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
-
-</html>
