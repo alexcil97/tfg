cebf722d34faf621703a926cd064ac788c2406e0
Alex_Cil <alejandro.ciljimenez@gmail.com>
2024-05-24 12:16:20 +0200

    commit pre analisis

--- Footer/Footer.php
+++ Footer/Footer.php
@@ -5,8 +5,8 @@
             <div class="row container-fluid m-1">
                 <div class="col-12 d-flex flex-wrap justify-content-around p-2">
                     <a href="../Home/home.php" class="btn btn-outline-dark m-2">Home</a>
-                    <a href="https://www.ejemplo.com/message" class="btn btn-outline-dark m-2">Message</a>
-                    <a href="https://www.ejemplo.com/search" class="btn btn-outline-dark m-2">Search</a>
+                    <a href="../Publication/publication.php" class="btn btn-outline-dark m-2">Publication</a>
+                    <a href="https://www.ejemplo.com/message" class="btn btn-outline-dark m-2">Search</a>
                     <a href="../Profile/profile.php" class="btn btn-outline-dark m-2">Profile</a>
                     <a href="https://www.ejemplo.com/search-company" class="btn btn-outline-dark m-2">Search Company</a>
                     <a href="https://www.ejemplo.com/classification" class="btn btn-outline-dark m-2">Classification</a>
